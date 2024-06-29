bc931c9dbf990490895451320a49f41bdce21666
shashj <sra.65943@gmail.com>
2024-06-30 00:09:54 +0530

    getting all records to json for train data

--- llama3/prep_dataset_i2b2.py
+++ llama3/prep_dataset_i2b2.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from tqdm import tqdm
 import json
 import os
 
@@ -7,34 +8,35 @@ file_path = '../datasets/i2b2/PHI_Processed_data/deid_surrogate_train_all_versio
 tree = ET.parse(file_path)
 root = tree.getroot()
 
-# Initialize dictionary to hold the JSON data
-data = {}
-
-# Extract the record ID
-record_id = root.find('.//RECORD').get('ID')
-data[record_id] = []
-
-# Find all PHI tags and append to the list
-phi_tags = root.findall('.//PHI')
-for phi in phi_tags:
-    phi_entry = {
-        'TYPE': phi.get('TYPE'),
-        'TEXT': phi.text
-    }
-    data[record_id].append(phi_entry)
-
-# Convert to JSON
-json_data = json.dumps(data, indent=4)
-
-
 # Define the output directory
 output_dir = '../datasets/i2b2/train_jsons'
 
 # Create the directory if it does not exist
 os.makedirs(output_dir, exist_ok=True)
 
-output_file_path = os.path.join(output_dir, f"{record_id}.json")
-with open(output_file_path, 'w') as json_file:
-    json_file.write(json_data)
+# Initialize dictionary to hold the JSON data
+data = {}
 
-print(f"JSON data saved to {output_file_path}")
+# Find all RECORD elements
+records = root.findall('.//RECORD')
+
+# Extract the record ID
+for record in tqdm(records, desc="Processing records"):
+    record_id = record.get('ID')
+    data[record_id] = []
+
+    # Find all PHI tags and append to the list
+    phi_tags = root.findall('.//PHI')
+    for phi in phi_tags:
+        phi_entry = {
+            'TYPE': phi.get('TYPE'),
+            'TEXT': phi.text
+        }
+        data[record_id].append(phi_entry)
+
+    # Convert to JSON
+    json_data = json.dumps(data, indent=4)
+
+    output_file_path = os.path.join(output_dir, f"{record_id}.json")
+    with open(output_file_path, 'w') as json_file:
+        json_file.write(json_data)
