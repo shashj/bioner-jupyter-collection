e7024ee5a21fb74d3c7f93fae6122df4792e1b85
shashj <sra.65943@gmail.com>
2024-07-28 21:42:16 +0530

    add finetune parameters

--- llama3/i2b2_finetune_dates.py
+++ llama3/i2b2_finetune_dates.py
@@ -133,6 +133,27 @@ def get_max_length(model):
         print(f"Using default max length: {max_length}")
     return max_length
 
+def print_trainable_parameters(model, use_4bit=False):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        num_params = param.numel()
+        # if using DS Zero 3 and the weights are initialized empty
+        if num_params == 0 and hasattr(param, "ds_numel"):
+            num_params = param.ds_numel
+
+        all_param += num_params
+        if param.requires_grad:
+            trainable_params += num_params
+    if use_4bit:
+        trainable_params /= 2
+    print(
+        f"all params: {all_param:,d} || trainable params: {trainable_params:,d} || trainable%: {100 * trainable_params / all_param}"
+    )
+
 ## Training
 
 ## check this too: https://blog.ovhcloud.com/fine-tuning-llama-2-models-using-a-single-gpu-qlora-and-ai-notebooks/
@@ -148,6 +169,9 @@ peft_config = LoraConfig(
         target_modules= ["q_proj", "k_proj", "v_proj", "o_proj",
                       "gate_proj", "up_proj", "down_proj",]
 )
+
+# Print information about the percentage of trainable parameters
+print_trainable_parameters(model)
 
 
 training_arguments = TrainingArguments(
